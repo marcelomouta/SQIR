0380b7981e4b8d011ff4d1dc6afb663861ec96a6
Marcelo Mouta <marcelomouta4@hotmail.com>
2023-02-10 18:15:08 +0000

    uncomment bell state matrix representation

--- examples/Bell.v
+++ examples/Bell.v
@@ -3,12 +3,12 @@ Local Open Scope ucom.
 
 Definition bell : base_ucom 2 := H 0; CNOT 0 1.
 
-(* Definition bell_state : Matrix (2^2) (1^2) :=
-1 / √2 .* ( ∣ 0,0 ⟩ .+ ∣ 1,1 ⟩). *)
+Definition bell_state : Matrix (2^2) (1^2) :=
+1 / √2 .* ( ∣ 0,0 ⟩ .+ ∣ 1,1 ⟩).
 
-Lemma bell_correct: uc_eval bell × ∣ 0,0 ⟩ = 1 / √2 .* ( ∣ 0,0 ⟩ .+ ∣ 1,1 ⟩).
+Lemma bell_correct: uc_eval bell × ∣ 0,0 ⟩ = bell_state.
 Proof.
-  simpl.
+  simpl. unfold bell_state.
   autorewrite with eval_db; simpl.
   solve_matrix.
 Qed.
